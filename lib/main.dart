b4bc60529a88cb54241ae12ba48f39511e5363d5
Khaled Osama <khaledosama@Khaleds-MacBook-Air.local>
2023-09-12 22:02:47 +0300

    trying

--- lib/main.dart
+++ lib/main.dart
@@ -1,89 +1,69 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:timezone/timezone.dart' as tz;
-import 'package:timezone/data/latest.dart' as tz;
 
-void main() {
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  const AndroidInitializationSettings initializationSettingsAndroid =
+      AndroidInitializationSettings('app_icon');
+
+  final InitializationSettings initializationSettings = InitializationSettings(
+    android: initializationSettingsAndroid,
+  );
+  await flutterLocalNotificationsPlugin.initialize(
+    initializationSettings,
+  );
+
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Notification salama',
-      home: NotificationPage(),
+      home: HomePage(),
     );
   }
 }
 
-class NotificationPage extends StatefulWidget {
-  @override
-  _NotificationPageState createState() => _NotificationPageState();
-}
-
-class _NotificationPageState extends State<NotificationPage> {
-  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
-      FlutterLocalNotificationsPlugin();
-
-  @override
-  void initState() {
-    super.initState();
-    initLocalNotifications();
-    scheduleNotification();
-  }
-
-  Future<void> initLocalNotifications() async {
-    const AndroidNotificationDetails androidPlatformChannelSpecifics =
-        AndroidNotificationDetails(
-      'channel_id',
-      'channel_name',
-      channelDescription: 'channel_description',
-      importance: Importance.max,
-      priority: Priority.high,
-    );
-
-    const NotificationDetails platformChannelSpecifics =
-        NotificationDetails(android: androidPlatformChannelSpecifics);
-
-    await flutterLocalNotificationsPlugin
-        .resolvePlatformSpecificImplementation<
-            AndroidFlutterLocalNotificationsPlugin>()
-        ?.createNotificationChannel(
-            androidPlatformChannelSpecifics as AndroidNotificationChannel);
-
-    tz.initializeTimeZones();
-  }
-
-  Future<void> scheduleNotification() async {
-    await flutterLocalNotificationsPlugin.zonedSchedule(
-      0, // unique identifier for the notification
-      'Title', // notification title
-      'Body', // notification body
-      tz.TZDateTime.now(tz.local)
-          .add(const Duration(seconds: 5)), // scheduled time
-      const NotificationDetails(),
-      androidScheduleMode: AndroidScheduleMode.exactAllowWhileIdle,
-      uiLocalNotificationDateInterpretation:
-          UILocalNotificationDateInterpretation.absoluteTime,
-    );
-  }
-
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Notification Demo'),
+        title: Text('Notification Example'),
       ),
       body: Center(
-        child: Text('Scheduled a notification'),
+        child: ElevatedButton(
+          onPressed: () {
+            showNotification();
+          },
+          child: Text('Show Notification'),
+        ),
       ),
+    );
+  }
+
+  Future<void> showNotification() async {
+    const AndroidNotificationDetails androidPlatformChannelSpecifics =
+        AndroidNotificationDetails(
+      'channel_id',
+      'channel_name',
+      importance: Importance.max,
+      priority: Priority.high,
+    );
+    const NotificationDetails platformChannelSpecifics =
+        NotificationDetails(android: androidPlatformChannelSpecifics);
+
+    await flutterLocalNotificationsPlugin.show(
+      0,
+      'Notification Title',
+      'Notification Body',
+      platformChannelSpecifics,
+      payload: 'notification_payload',
     );
   }
 }
